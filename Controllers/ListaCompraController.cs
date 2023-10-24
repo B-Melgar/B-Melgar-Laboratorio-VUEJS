0d7d1cf0b55eb7d2c63956e6b9e9af07b9a1b4cc
Alex Melgar <melgarbryan778@gmail.com>
2023-10-24 00:02:35 -0600

    Laboratorio Crud utilizando Aps.net y Vue.js

---
+++ Controllers/ListaCompraController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class ListaCompraController : Controller
+{
+    private const string ApiUrl = "https://tuservicioweb.com/api/listacompras";
+
+    public async Task<IActionResult> Index()
+    {
+        try
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(ApiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsAsync<List<ListaCompra>>();
+                    return View(data);
+                }
+                else
+                {
+                    // Manejo de errores si la solicitud no fue exitosa
+                    // Puedes redirigir a una vista de error, mostrar un mensaje, etc.
+                    return View(new List<ListaCompra>());
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Manejo de errores si ocurre una excepción durante la solicitud
+            // Puedes redirigir a una vista de error, mostrar un mensaje, etc.
+            return View(new List<ListaCompra>());
+        }
+    }
+}
